d19068a1e1039c24f080bc1a8e7ecb1e0edaaa45
TYDTYD <48386074+TYDTYD@users.noreply.github.com>
2025-01-22 00:52:33 +0900

    세팅 UI 위아래 이동 구현
    하이라이트 색깔 수정

--- Assets/Scripts/UI/Setting_Ui.cs
+++ Assets/Scripts/UI/Setting_Ui.cs
@@ -6,33 +6,37 @@ using System.Collections;
 
 public class Setting_Ui : MonoBehaviour
 {
-    WaitForSeconds seconds = new WaitForSeconds(2f);
     [SerializeField] Button[] GetButtons;
-    [SerializeField] VerticalLayoutGroup group;
-    float start, end;
+    [SerializeField] RectTransform group;
+    int pos = 0;
+    Vector2 CachePosition;
+    float initPosition;
+    ColorBlock colorVar;
+    ColorBlock original;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        //GetButtons[0].getcom
+        colorVar = GetButtons[pos].colors;
+        original = GetButtons[pos].colors;
+        colorVar.normalColor = new Color(80/255f, 80/255f, 80 / 255f);
+        CachePosition = group.anchoredPosition;
+        initPosition = group.anchoredPosition.y;
+        GetButtons[pos].colors = colorVar;
     }
-
     private IEnumerator MoveUI(float start, float end)
     {
         float time = 0;
-        float duration = 1f;
+        float duration = 0.5f;
         while (time<duration)
         {
             float t = time / duration;
             t = Mathf.Sin((t * Mathf.PI) / 2);
-
             float currentValue = Mathf.Lerp(start, end, t);
-
             time += Time.deltaTime;
-            Debug.Log(currentValue);
+            CachePosition.y = currentValue;
+            group.anchoredPosition = CachePosition;
             yield return null;
         }
-
     }
 
     // Update is called once per frame
@@ -41,11 +45,25 @@ public class Setting_Ui : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            StartCoroutine(MoveUI(0, 1));
+            pos = pos < GetButtons.Length - 1 ? pos + 1 : pos;
+            GetButtons[pos].colors = colorVar;
+            GetButtons[pos - 1].colors = original;
+            if (pos > 3 && pos<GetButtons.Length-1)
+            {
+                float start = group.anchoredPosition.y;
+                StartCoroutine(MoveUI(start, initPosition + 60 * (pos - 3)));
+            }
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-
+            pos = pos > 0 ? pos - 1 : pos;
+            GetButtons[pos].colors = colorVar;
+            GetButtons[pos + 1].colors = original;
+            if (pos >= 3)
+            {
+                float start = group.anchoredPosition.y;
+                StartCoroutine(MoveUI(start, initPosition + 60 * (pos - 3)));
+            }
         }
     }
 }
