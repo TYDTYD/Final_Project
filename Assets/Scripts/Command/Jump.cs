c467f417dbb3108758a0749d4ea62589432532e1
TYDTYD <48386074+TYDTYD@users.noreply.github.com>
2025-05-10 18:34:42 +0900

    거미 몬스터 구현

--- Assets/Scripts/Command/Jump.cs
+++ Assets/Scripts/Command/Jump.cs
@@ -11,6 +11,14 @@ public class Jump : ICommand
         JumpForce = force;
         Direction = new Vector2(0, JumpForce);
     }
+
+    public Jump(Rigidbody2D rigidbody, float force, Vector2 dir)
+    {
+        Rigidbody2D = rigidbody;
+        JumpForce = force;
+        Direction = dir + new Vector2(0, JumpForce);
+    }
+
     public void Execute()
     {
         Rigidbody2D.AddForce(Direction, ForceMode2D.Impulse);
---
+++ Assets/Scripts/Monster/Spider/Spider.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using System.Collections;
+
+public class Spider : MonoBehaviour, IHealth
+{
+    int health = 1;
+    bool reversed = true;
+    float detectDistance = 5f;
+    float jumpDelay = 1.5f;
+    float jumpForce = 17f;
+    bool canJump = true;
+    GameObject player;
+    ICommand jump;
+    Rigidbody2D GetRigidbody;
+    private void Awake()
+    {
+        GetRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        player = GameManager.Instance.GetPlayer;
+    }
+
+    private void OnEnable() => UpdateManager.Instance.SubscribeUpdate(UpdateMethod);
+    private void OnDisable() => UpdateManager.Instance.UnSubscribeUpdate(UpdateMethod);
+    void UpdateMethod()
+    {
+        if (reversed)
+            CheckPlayer();
+        else
+            FollowPlayer();
+    }
+
+    void CheckPlayer()
+    {
+        RaycastHit2D[] raycast = Physics2D.RaycastAll(transform.position, Vector2.down, detectDistance);
+
+        foreach(var ray in raycast)
+        {
+            if (ray.collider.CompareTag("Player"))
+            {
+                reversed = false;
+                transform.rotation = Quaternion.Euler(Vector2.zero);
+                GetRigidbody.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+
+    void FollowPlayer()
+    {
+        if (canJump)
+        {
+            if (player != null)
+            {
+                Vector2 direction = (player.transform.position - transform.position).normalized;
+                jump = new Jump(GetRigidbody, jumpForce, direction);
+                StartCoroutine(ExecuteJump());
+            }
+        }
+    }
+
+    IEnumerator ExecuteJump()
+    {
+        canJump = false;
+        jump.Execute();
+        yield return new WaitForSeconds(jumpDelay);
+        canJump = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if(collision.gameObject.TryGetComponent(out IHealth health))
+            {
+                foreach (ContactPoint2D contact in collision.contacts)
+                {
+                    Vector2 normal = contact.normal; // 충돌한 표면의 방향 (노멀 벡터)
+                    if (normal.y < -0.5f)
+                    {
+                        if (collision.gameObject.TryGetComponent(out Rigidbody2D playerRb))
+                            playerRb.AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
+                        gameObject.SetActive(false);
+                        return;
+                    }
+                }
+                health.TakeDamage(1, 300, gameObject);
+            }
+        }
+    }
+
+    public void TakeDamage(int damage, int force, GameObject obj)
+    {
+        health -= damage;
+        gameObject.SetActive(false);
+    }
+
+    public void Heal(int amount)
+    {
+        return;
+    }
+}
